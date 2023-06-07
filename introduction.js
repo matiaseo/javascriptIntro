c43d04c517c37b495338a883277d5a39029fd785
matiaseo <matiaseolguin@gmail.com>
2023-06-06 23:46:33 -0300

    Improve card styles and random generator

--- introduction.js
+++ introduction.js
@@ -1,12 +1,26 @@
 ;(() => {
-const predefinedCats = []
+
+const getRandomTraits = () => [
+    { name: Math.random().toString(36).replace(/\.|\d/g,''), type: ['quiet', 'annoying'][(Math.random()<.5)|0] },
+    { name: Math.random().toString(36).replace(/\.|\d/g,''), type: ['quiet', 'annoying'][(Math.random()<.5)|0] },
+    { name: Math.random().toString(36).replace(/\.|\d/g,''), type: ['quiet', 'annoying'][(Math.random()<.5)|0] },
+].slice(0, Math.round(1 + Math.random()*2))
+const predefinedCats = Array(8).fill(0).map(()=>({
+    name: Math.random().toString(36).replace(/\.|\d/g,''),
+    breed: Math.random().toString(36).replace(/\.|\d/g,''),
+    age: Math.round(Math.random()*20),
+    size: Math.random().toString(36).replace(/\.|\d/g,''),
+    picture: 'https://cataas.com/cat/CVB6GXU26EYzCldX?width=96&height=96',
+    traits: getRandomTraits(),
+    cardColor: '#'+Math.random().toString(16).slice(2,5)
+}))
 
 const cats = [{
     name: 'Raiz',
     breed: 'black',
     age: 1,
     size: 'large',
-    picture: 'https://cataas.com/cat/CVB6GXU26EYzCldX?width=64&height=64',
+    picture: 'https://cataas.com/cat/CVB6GXU26EYzCldX?width=96&height=96',
     traits: [
         { name: 'sleepy', type: 'quiet' },
         { name: 'grumpy', type: 'annoying' }
@@ -18,7 +32,7 @@ const allCats = cats.concat(predefinedCats)
 renderList("All cats", allCats)
 
 // filter
-const olderCats = allCats.filter(({age}) => age > 5)
+const olderCats = allCats.filter(({age}) => age > 15)
 renderList("Filter older cats", olderCats)
 
 // every
--- main.js
+++ main.js
@@ -1,50 +1,6 @@
 ;(() => {
     const resultContainer = document.querySelector('.resultContainer')
 
-    const productsUrl = 'https://unqctexfi6z2hc4is5sitgbvxy0gmbjz.lambda-url.us-east-2.on.aws/'
-    let products = []
-    const toProductCard = ({ name, description, price, image }) => {
-        const li = document.createElement('li')
-        const figure = document.createElement('figure')
-        const img = Object.assign(document.createElement('img'), {
-            src: image,
-            className: 'productImage'
-        })
-        const duppedImage = Object.assign(document.createElement('img'), {
-            src: image,
-            className: 'duppedImage'
-        })
-        const figcaption = Object.assign(document.createElement('figcaption'), {
-            textContent: name
-        })
-        const span = Object.assign(document.createElement('span'), {
-            textContent: description,
-            className: 'productDescription'
-        })
-        const b = Object.assign(document.createElement('b'), {
-            textContent: `$${price}`,
-            className: 'price'
-        })
-        figure.appendChild(duppedImage)
-        figure.appendChild(img)
-        figure.appendChild(figcaption)
-        li.appendChild(figure)
-        li.appendChild(span)
-        li.appendChild(b)
-        return li
-    }
-    const renderProducts = products => {
-        resultContainer.innerHTML = ''
-        products.map(toProductCard)
-            .forEach(productCard => {
-                resultContainer.appendChild(productCard)
-            });
-    }
-    // (async () => {
-    //     renderProducts(products = await (await fetch(productsUrl)).json())
-    // })()
-
-
     const toTraitPill = ({ name, type }) => {
         return Object.assign(document.createElement('span'), {
             textContent: name,
@@ -53,12 +9,18 @@
     }
 
     const toCatCard = ({ name, picture, breed, age, size, traits, cardColor }) => {
-        const li = document.createElement('li')
-        if(cardColor) li.style.setProperty('--cardColor', cardColor)
+        const li = Object.assign(document.createElement('li'), {
+            className: 'catCard'
+        })
+        if(cardColor) {
+            li.style.setProperty('--cardColor', cardColor+'4')
+            li.style.setProperty('--cardShadowColor', cardColor)
+        }
+
         const figure = document.createElement('figure')
         const img = Object.assign(document.createElement('img'), {
             src: picture,
-            className: 'productImage'
+            className: 'catImage'
         })
         const duppedImage = Object.assign(document.createElement('img'), {
             src: picture,
@@ -67,23 +29,38 @@
         const figcaption = Object.assign(document.createElement('figcaption'), {
             textContent: name
         })
+        figcaption.style.setProperty('--imageWidth', `96px`)
         const breedSpan = Object.assign(document.createElement('span'), {
+            textContent: 'Breed: '
+        })
+        const breedB = Object.assign(document.createElement('b'), {
             textContent: breed
         })
         const ageSpan = Object.assign(document.createElement('span'), {
+            textContent: 'Age: '
+        })
+        const ageB = Object.assign(document.createElement('b'), {
             textContent: age
         })
         const sizeSpan = Object.assign(document.createElement('span'), {
+            textContent: 'Size: '
+        })
+        const sizeB = Object.assign(document.createElement('b'), {
             textContent: size
         })
 
-        const traitsContainer = document.createElement('div')
+        const traitsContainer = Object.assign(document.createElement('div'), {
+            className: 'traitsContainer'
+        })
         traits.map(toTraitPill)
             .forEach(traitPill => traitsContainer.appendChild(traitPill))
 
         figure.appendChild(duppedImage)
         figure.appendChild(img)
         figure.appendChild(figcaption)
+        breedSpan.appendChild(breedB)
+        ageSpan.appendChild(ageB)
+        sizeSpan.appendChild(sizeB)
         li.appendChild(figure)
         li.appendChild(breedSpan)
         li.appendChild(ageSpan)
@@ -92,24 +69,41 @@
         return li
     }
 
+    const getNoContentElement = () => {
+        const container = Object.assign(document.createElement('div'), {
+            className: 'noContent'
+        })
+        const span1 = Object.assign(document.createElement('span'), {
+            textContent: 'What a CATastrophe!',
+        })
+        const span2 = Object.assign(document.createElement('span'), {
+            textContent: 'No cats to render',
+        })
+        container.appendChild(span1)
+        container.appendChild(span2)
+        return container
+    }
+
     const getStepResult = (title, list) => {
-        const li = document.createElement('li')
-        const titleElement = Object.assign(document.createElement('div'), {
+        const li = Object.assign(document.createElement('ul'), {
+            className: 'stepResultSectionContainer'
+        })
+        const titleElement = Object.assign(document.createElement('header'), {
             textContent: title,
             className: 'stepResultTitle'
         })
-        const ul = Object.assign(document.createElement('ul'), {
-            className: 'stepResultContainer'
+        const stepResultListContainer = Object.assign(document.createElement('ul'), {
+            className: 'stepResultListContainer'
         })
         li.appendChild(titleElement)
         list.map(toCatCard)
-            .forEach(catCard => ul.appendChild(catCard))
+            .forEach(catCard => stepResultListContainer.appendChild(catCard))
 
-        if(!list?.length)
-            li.appendChild(Object.assign(document.createElement('span'), {
-                textContent: 'What a CATastrophe! No cats to render'
-            }))
-        li.appendChild(ul)
+        if(!list?.length) {
+            li.appendChild(getNoContentElement())
+        } else {
+            li.appendChild(stepResultListContainer)
+        }
         return li
     }
 
